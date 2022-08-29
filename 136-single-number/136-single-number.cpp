8f10df7a7b630372b797432f6f434f35c5cdae5e
Arpit Jain <55352601+arpit456jain@users.noreply.github.com>
2022-08-29 19:33:05 +0530

    Time: 45 ms (24.52%), Space: 20.7 MB (5.06%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         map<int,int> m;
+        for(auto i : nums)
+            m[i]++;
+        
+        vector<int> ans;
+        for(auto i : m)
+        {
+            if(i.second == 1)
+                return (i.first);
+        }
+        return -1;
+    }
+};
