0ce0d6c78ee766e07349de4f8235a894af204d3c
Arpit Jain <55352601+arpit456jain@users.noreply.github.com>
2022-08-31 13:59:36 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.94%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int setBits(int N) {
+        // Write Your Code here
+      
+        int count=0;
+        while(N!=0)
+        {
+           
+           N = N&(N-1);
+            count++;
+            
+            
+        }
+        
+        return count;
+    }
+    int hammingDistance(int x, int y) {
+        
+        return setBits(x xor y);
+    }
+};
