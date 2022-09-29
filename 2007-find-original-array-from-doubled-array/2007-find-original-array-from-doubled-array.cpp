8cac72e3cda1e6ecb09d9fe5704a7a0efafdee33
Arpit Jain <55352601+arpit456jain@users.noreply.github.com>
2022-09-29 11:30:34 +0530

    Time: 664 ms (75.77%), Space: 146.4 MB (26.87%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& ch) {
+        int n = ch.size();
+        if(n%2==1)
+            return {};
+        
+        map<int,int> m;
+        for(auto i : ch)
+            m[i]++;
+        
+        vector<int> ans;
+        sort(ch.begin(),ch.end());
+        
+        for(auto i : ch)
+        {
+            if(m.find(i) != m.end() and m[i] == 0)
+                continue;
+            
+            if(m.find(2*i) != m.end())
+            {
+                if(m[2*i] == 0)
+                    return {};
+            }
+            
+            
+            ans.push_back(i);
+            m[i]--;
+            m[2*i]--;
+                
+        }
+        if(ans.size() == n/2)
+            return ans;
+        else
+            return {};
+        
+    }
+};
