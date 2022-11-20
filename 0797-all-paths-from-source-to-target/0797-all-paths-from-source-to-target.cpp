b107b3095c0ab1beaf08ddf332c6e11e70fe7493
Arpit Jain <55352601+arpit456jain@users.noreply.github.com>
2022-11-20 22:56:05 +0530

    Time: 23 ms (65.34%), Space: 10.6 MB (81.78%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        // we already have adj matrix
+        
+        vector<int> temp;
+        vector<vector<int>> ans;
+        vector<int> vis(n+1,-1);
+        findpath(ans,temp,0,vis,n,graph);
+        
+        return ans;
+        
+    }
+    
+    void findpath(vector<vector<int>> &ans,vector<int> &tmp,int cur,vector<int> &vis,int n,vector<vector<int>>& graph)
+    {
+        if(cur == n-1)
+        {
+            tmp.push_back(cur);
+            ans.push_back(tmp);
+            tmp.pop_back();
+        }
+        
+        if(vis[cur]!=-1)
+            return;
+        
+        vis[cur]=1;
+        tmp.push_back(cur);
+        for(auto i : graph[cur])
+        {
+            findpath(ans,tmp,i,vis,n,graph);
+        }
+        vis[cur] = -1;
+        tmp.pop_back();
+        return;
+    }
+    
+};
